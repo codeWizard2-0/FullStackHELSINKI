423f6b1c806fa54eccce2bdbf7cd831411e422f8
Bishwo karki <bishwokarki@unomaha.edu>
2021-02-01 15:11:42 -0600

    1.3-1.5 changes updated

--- src/index.js
+++ src/index.js
@@ -10,16 +10,16 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <>
-      <p> {props.part} {props.exercise} </p>
+      <p> {props.parts.name} {props.parts.exercise} </p>
     </>
   );
 }
 const Content = (props) => {
   return (
     <>
-      <Part  part = {props.parts[0].name} exercise = {props.parts[0].exercises} />
-      <Part part = {props.parts[1].name} exercise = {props.parts[1].exercises} />
-      <Part part = {props.parts[2].name} exercise = {props.parts[2].exercises} />
+      <Part  part = {props.parts[0]} />
+      <Part part = {props.parts[1]} />
+      <Part part = {props.parts[2]} />
     </>
   );
 }
